4d7dde56a5be59aaae15d84977a4f1da196f2a81
1204848 <j.c.1983@live.com>
2015-11-09 14:56:27 +0000

    Initial commit

---
+++ Content.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1204848
+ * Date: 09/11/2015
+ * Time: 13:54
+ */
+
+$query = $_GET["query"];
+
+if ($query == "JR"){
+}
---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Wiki Page</title>
+</head>
+<body>
+
+<a href="http://1204848-lab1.azurewebsites.net/index.php?sectionID=1">Jack the Ripper</a><br /><br />
+
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1204848
+ * Date: 09/11/2015
+ * Time: 14:48
+ */
